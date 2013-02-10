f72d3f8705a058df3f71aebdaaf810fdcf2f451e
Phil Corbett <corbett.phil@gmail.com>
2013-02-09 20:15:26 -0500

    include tone to hz calculation

--- js/8bitSynthesizer.js
+++ js/8bitSynthesizer.js
@@ -1,7 +1,38 @@
 var Syn = (function () {
-	var that = this;
+	var that = this,
+		_A4 = {
+			tone: 'A',
+			hz: 440.0,
+			octive: 4,
+			duration: 'Quarter',
+			ms: 333
+		},
+		_note = {
+			'A': 10,
+			'A#': 11,
+			'B': 12,
+			'C': 1,
+			'C#': 2,
+			'D': 3,
+			'D#': 4,
+			'E': 5,
+			'F': 6,
+			'F#': 7,
+			'G': 8,
+			'G#': 9
+		};
+	
+	function _CalcHz(note) {
+		return Math.pow(2, (_CalcHalfSteps(_A4, note) / 12.0)) * _A4.hz;
+	}
+	
+	function _CalcHalfSteps(n1, n2) {
+		return (_note[n2.tone] + (n2.octive * 12)) - (_note[n1.tone] + (n1.octive * 12));
+	}
 	
 	that.note = function (o) {
+		o.octive = o.octive || 4;
+		o.hz = _CalcHz(o);
 		return o;
 	};
 	
--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 module('V 0.1.0');
+
 test('Note Created', function () {
-	var note = Syn.note({tone: 'A', duration: 'Quarter'});
+	var note = Syn.note({tone: 'A', duration: 'Quarter', octive: 4});
 	
 	equal(note.tone, 'A', 'Tone Passed');
 	equal(note.duration, 'Quarter', 'Duration Passed');
+	equal(note.octive, 4, 'Octive Passed');
 });
 
 test('Track Created', function () {
@@ -34,4 +36,29 @@ test('Sheet Create', function() {
 	equal(sheet.tracks.length, 0, 'Notes Passed');
 });
 
-// V 0.0.2
+module('V 0.2.0');
+
+test('Note normalized', function () {	
+	equal(Syn.note({tone: 'A', octive: 3}).hz.toFixed(2), 220.00, 'A Tone Translated');
+	equal(Syn.note({tone: 'A#', octive: 3}).hz.toFixed(2), 233.08, 'A# Tone Translated');
+	equal(Syn.note({tone: 'B', octive: 3}).hz.toFixed(2), 246.94, 'B Tone Translated');
+	equal(Syn.note({tone: 'C'}).hz.toFixed(2), 261.63, 'C Tone Translated');
+	equal(Syn.note({tone: 'C#'}).hz.toFixed(2), 277.18, 'C# Tone Translated');
+	equal(Syn.note({tone: 'D'}).hz.toFixed(2), 293.66, 'D Tone Translated');
+	equal(Syn.note({tone: 'D#'}).hz.toFixed(2), 311.13, 'D# Tone Translated');
+	equal(Syn.note({tone: 'E'}).hz.toFixed(2), 329.63, 'E Tone Translated');
+	equal(Syn.note({tone: 'F'}).hz.toFixed(2), 349.23, 'F Tone Translated');
+	equal(Syn.note({tone: 'F#'}).hz.toFixed(2), 369.99, 'F# Tone Translated');
+	equal(Syn.note({tone: 'G'}).hz.toFixed(2), 392.00, 'G Tone Translated');
+	equal(Syn.note({tone: 'G#'}).hz.toFixed(2), 415.30, 'G# Tone Translated');
+	equal(Syn.note({tone: 'A'}).hz.toFixed(2), 440.00, 'A Tone Translated');
+	equal(Syn.note({tone: 'A#'}).hz.toFixed(2), 466.16, 'A# Tone Translated');
+	equal(Syn.note({tone: 'B'}).hz.toFixed(2), 493.88, 'B Tone Translated');
+	equal(Syn.note({tone: 'C', octive: 5}).hz.toFixed(2), 523.25, 'C Tone Translated');
+	
+	equal(Syn.note({duration: 'Sixteenth'}).ms, 83, 'Duration ms');
+	equal(Syn.note({duration: 'Eighth'}).ms, 167, 'Duration ms');
+	equal(Syn.note({duration: 'Quarter'}).ms, 333, 'Duration ms');
+	equal(Syn.note({duration: 'Half'}).ms, 667, 'Duration ms');
+	equal(Syn.note({duration: 'Whole'}).ms, 1333, 'Duration ms');
+});
